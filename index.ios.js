6b9952e62d61784641946e0737aa9eb5b552b6b7
Gertjan Reynaert <gertjan@typework.com>
2017-02-23 19:42:49 +0100

    Add first implementation of redux

--- index.ios.js
+++ index.ios.js
@@ -9,10 +9,89 @@ import { AppRegistry } from 'react-native';
 
 import TodoList from './src/TodoList';
 
+const createStore = (reducer, initialState = {}) => {
+  let state = initialState;
+  const listeners = [];
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  };
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+  };
+
+  const getState = () => state;
+
+  return { dispatch, subscribe, getState };
+};
+
+const initialState = {
+  todos: [],
+  latestId: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO': {
+      const newId = state.latestId + 1;
+      const newTodo = {
+        id: newId,
+        ...action.payload,
+      };
+
+      return {
+        todos: [...state.todos, newTodo],
+        latestId: newId,
+      };
+    }
+    case 'EDIT_TODO': {
+      const index = state.todos.findIndex(todo =>
+        todo.id === action.payload.id,
+      );
+      const todo = state.todos[index];
+      const editedTodo = {
+        ...todo,
+        ...action.payload,
+      };
+
+      return {
+        ...state,
+        todos: [
+          ...state.todos.slice(0, index),
+          editedTodo,
+          ...state.todos.slice(index + 1),
+        ],
+      };
+    }
+    case 'DELETE_TODO': {
+      const indexToRemove = state.todos.findIndex(todo =>
+        todo.id === action.payload.id,
+      );
+
+      return {
+        ...state,
+        todos: [
+          ...state.todos.slice(0, indexToRemove),
+          ...state.todos.slice(indexToRemove + 1),
+        ],
+      };
+    }
+    default: return state;
+  }
+};
+
+const store = createStore(reducer, initialState);
+
 export default class todo extends Component {
   render() {
     return (
-      <TodoList />
+      <TodoList
+        dispatch={store.dispatch}
+        subscribe={store.subscribe}
+        getState={store.getState}
+      />
     );
   }
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -4,8 +4,6 @@ import { View, Text, ListView, StyleSheet } from 'react-native';
 import Button from './Button';
 import TodoItem from './TodoItem';
 
-let counterId = 23;
-
 const white = 'white';
 const styles = StyleSheet.create({
   wrapper: {
@@ -25,78 +23,56 @@ const styles = StyleSheet.create({
   },
 });
 
+type Props = {
+  subscribe: () => void,
+  getState: () => void,
+  dispatch: () => void,
+};
+
 class TodoList extends Component {
+  props: Props
+
   constructor(props) {
     super(props);
-    this.state = {
-      todos: [
-        { id: 23, selected: false, text: 'scroll' },
-        { id: 22, selected: false, text: 'to' },
-        { id: 21, selected: false, text: 'need' },
-        { id: 20, selected: false, text: "I'll" },
-        { id: 19, selected: false, text: 'before' },
-        { id: 18, selected: false, text: 'need' },
-        { id: 17, selected: false, text: "i'll" },
-        { id: 16, selected: false, text: 'items' },
-        { id: 15, selected: false, text: 'much' },
-        { id: 14, selected: false, text: 'how' },
-        { id: 13, selected: false, text: 'sure' },
-        { id: 12, selected: false, text: 'not' },
-        { id: 11, selected: false, text: "i'm" },
-        { id: 10, selected: false, text: 'but' },
-        { id: 9, selected: false, text: 'Scrollview' },
-        { id: 8, selected: false, text: 'A' },
-        { id: 7, selected: false, text: 'With' },
-        { id: 6, selected: false, text: 'Develop' },
-        { id: 5, selected: false, text: 'To' },
-        { id: 4, selected: false, text: 'Test' },
-        { id: 3, selected: false, text: 'A' },
-        { id: 2, selected: false, text: 'Is' },
-        { id: 1, selected: false, text: 'This' },
-      ],
-    };
+    this.state = this.props.getState();
   }
 
   componentWillMount() {
+    this.props.subscribe(() => {
+      const newState = this.props.getState();
+
+      this.setState(newState);
+    });
+
     this.dataSource = new ListView.DataSource({
       rowHasChanged: (row1, row2) => row1 !== row2,
     });
-  }
-
-  addTodoItem() {
-    counterId += 1;
-
-    const newTodo = { id: counterId, selected: false, text: '' };
-
-    this.setState({
-      todos: [newTodo, ...this.state.todos],
-    });
-  }
-
-  changeTodoItem(id, values) {
-    const newTodos = this.state.todos.map((todo) => {
-      if (todo.id !== id) return todo;
-
-      return {
-        ...todo,
-        ...values,
-      };
-    });
-    this.setState({ todos: newTodos });
-  }
-
-  removeTodoItem(id) {
-    const newTodos = this.state.todos.filter(todo => todo.id !== id);
-    this.setState({ todos: newTodos });
   }
 
   render() {
     console.log('State:', JSON.stringify(this.state, null, 2));
     const todos = this.dataSource.cloneWithRows(this.state.todos);
 
+    const dispatchAdd = () => this.props.dispatch({
+      type: 'ADD_TODO',
+      payload: {
+        text: '',
+      },
+    });
+
+    const dispatchEdit = (id, values) => this.props.dispatch({
+      type: 'EDIT_TODO',
+      payload: { id, ...values },
+    });
+
+    const dispatchDelete = id => this.props.dispatch({
+      type: 'DELETE_TODO',
+      payload: { id },
+    });
+
     return (
       <View style={styles.wrapper}>
-        <Button onPress={() => this.addTodoItem()} style={styles.buttonStyle}>
+        <Button onPress={dispatchAdd} style={styles.buttonStyle}>
           <Text style={styles.addText}>Add</Text>
         </Button>
         {
@@ -112,8 +88,8 @@ class TodoList extends Component {
                       key={id}
                       selected={selected}
                       text={text}
-                      onChange={values => this.changeTodoItem(id, values)}
-                      onDelete={() => this.removeTodoItem(id)}
+                      onChange={values => dispatchEdit(id, values)}
+                      onDelete={() => dispatchDelete(id)}
                     />
                   )
                 }
